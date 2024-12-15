73596f4ee742804c2d2de2bbacb8ee29ce1c3a36
matvik777 <2805matvik4@gmail.com>
2024-12-16 02:01:13 +0300

    B: изменен main.py, считает слова а не выводит текст

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def main():
     path_to_file = "books/frankenstein.txt"
     with open(path_to_file, "r", encoding="utf-8") as f:
-        file_contents = f.read()
+        file_contents = len(f.read().split())
     print(file_contents)
     
 if __name__ == "__main__":
